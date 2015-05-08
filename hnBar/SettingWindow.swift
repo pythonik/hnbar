5b9c99fc60bc94da577b7d7349c83559a9597b2f
kun <kunsupython@gmail.com>
2015-05-08 01:16:44 -0500

    setting window control

---
+++ hnBar/SettingWindow.swift
@@ -0,0 +1,19 @@
+//
+//  SettingWindow.swift
+//  hnBar
+//
+//  Created by Kun Su on 2015-05-08.
+//  Copyright (c) 2015 Kun Su. All rights reserved.
+//
+
+import Cocoa
+
+class SettingWindow: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
