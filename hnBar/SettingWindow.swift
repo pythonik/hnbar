2a7698661fbf54f6c68f1218a5fa3dab0e80d477
kun <kunsupython@gmail.com>
2015-05-14 15:02:14 -0500

    validation

--- hnBar/SettingWindow.swift
+++ hnBar/SettingWindow.swift
@@ -7,27 +7,51 @@
 //
 
 import Cocoa
+import AppKit
 
 class SettingWindow: NSWindowController {
+    
+    
+    @IBOutlet weak var interest: NSTextField!
     
     var managedObjectContext: NSManagedObjectContext? = {
         let appDelegate = NSApplication.sharedApplication().delegate as! AppDelegate
         return appDelegate.managedObjectContext!
         
     }()
-       
+    
     override func windowDidLoad() {
-        
+        interest.formatter = OnlyNumber()
         super.windowDidLoad()
         
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
         
     }
     
-    @IBAction func addClicked(sender: AnyObject) {
-        println("add")
+    @IBAction func removeAllTag(sender: AnyObject) {
+        println("remove")
+        
     }
     
-    @IBAction func removeClicked(sender: AnyObject) {
+}
+
+class OnlyNumber: NSFormatter {
+    
+    override func stringForObjectValue(obj: AnyObject) -> String? {
+        return nil
     }
+    
+    override func getObjectValue(obj: AutoreleasingUnsafeMutablePointer<AnyObject?>, forString string: String, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>) -> Bool {
+        return true
+    }
+    
+    override func isPartialStringValid(partialString: String, newEditingString newString: AutoreleasingUnsafeMutablePointer<NSString?>, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>) -> Bool {
+        if(partialString.rangeOfCharacterFromSet(NSCharacterSet.decimalDigitCharacterSet().invertedSet) != nil) {
+            NSBeep()
+            return false
+        }
+        
+        return true
+    }
+    
 }
